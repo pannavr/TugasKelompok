d7873ec94d5a1f10c1957b4a155c41b5bbe302a6
yogigan <bachtiarnuryogipratama1@gmail.com>
2020-09-11 20:54:05 +0700

    Ganti Nama Class

---
+++ tugaskelompok/src/tugaskelompok/Main.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaskelompok;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int inputPilihan;
+        boolean pilihan = true;
+        while (pilihan) {
+            System.out.println("=====================");
+            System.out.println("1. Add");
+            System.out.println("2. View");
+            System.out.println("3. Login");
+            System.out.println("=====================");
+            System.out.print("Pilihan(1-3) : ");
+            inputPilihan = scan.nextInt();
+            switch (inputPilihan) {
+                case 1:
+                    break;
+                case 2:
+                    System.out.println("2");
+                    break;
+                case 3:
+                    System.out.println("3");
+                    break;
+                default:
+                    System.out.println("Data tidak di temukan silahkan pilih 1-3");
+            }
+        }
+    }
+
+}
