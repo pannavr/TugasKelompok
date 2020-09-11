11352b91b9f0fc21f9f1e33f0b31abaeb7bdaf3d
yogigan <bachtiarnuryogipratama1@gmail.com>
2020-09-11 21:51:11 +0700

    Revert "Employees class Setter & getter"
    This reverts commit a2f519704e5dc59fd805776944d96e0b8aafcb14.

--- tugaskelompok/src/tugaskelompok/Employees.java
+++ tugaskelompok/src/tugaskelompok/Employees.java
@@ -10,46 +10,5 @@ package tugaskelompok;
  * @author NAME
  */
 public class Employees {
-    int idEmployee;
-    String namaDepan,namaBelakang,username,password;
-
-    public int getIdEmployee() {
-        return idEmployee;
-    }
-
-    public void setIdEmployee(int idEmployee) {
-        this.idEmployee = idEmployee;
-    }
-
-    public String getNamaDepan() {
-        return namaDepan;
-    }
-
-    public void setNamaDepan(String namaDepan) {
-        this.namaDepan = namaDepan;
-    }
-
-    public String getNamaBelakang() {
-        return namaBelakang;
-    }
-
-    public void setNamaBelakang(String namaBelakang) {
-        this.namaBelakang = namaBelakang;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    
 }
