e8022927d4f3f0036198d64b48e63086d62826ff
yogigan <bachtiarnuryogipratama1@gmail.com>
2020-09-11 21:17:11 +0700

    Tambah Employees class

--- tugaskelompok/src/tugaskelompok/Main.java
+++ tugaskelompok/src/tugaskelompok/Main.java
@@ -20,6 +20,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int inputPilihan;
         boolean pilihan = true;
+
         while (pilihan) {
             System.out.println("=====================");
             System.out.println("1. Add");
@@ -28,14 +29,33 @@ public class Main {
             System.out.println("=====================");
             System.out.print("Pilihan(1-3) : ");
             inputPilihan = scan.nextInt();
+
             switch (inputPilihan) {
                 case 1:
+                    System.out.println("========================================"); 
+                    System.out.println("Pilihan(1-3) : 1");
+                    System.out.println("Nama Lengkap    : ");
+                    System.out.println("Password        :");
+                    System.out.println("========================================");
+                    System.out.println("");
                     break;
                 case 2:
-                    System.out.println("2");
+                    System.out.println("========================================");
+                    System.out.println("Pilihan(1-3) : 2");
+                    System.out.println("Id Karyawan     : ");
+                    System.out.println("Nama Depan      : ");
+                    System.out.println("Nama Belakang   : ");
+                    System.out.println("Username        : ");
+                    System.out.println("========================================");
+                    System.out.println("");
                     break;
                 case 3:
-                    System.out.println("3");
+                    System.out.println("========================================");
+                    System.out.println("Pilihan(1-3) : 3");
+                    System.out.println("Username    : ");
+                    System.out.println("Password    : ");
+                    System.out.println("========================================");
+                    System.out.println("");
                     break;
                 default:
                     System.out.println("Data tidak di temukan silahkan pilih 1-3");
