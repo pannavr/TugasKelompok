4dd4fd2d9d956a6466d2b8ffbe0d5ee72133367b
yogigan <bachtiarnuryogipratama1@gmail.com>
2020-09-12 13:37:50 +0700

    Case 2

--- tugaskelompok/src/tugaskelompok/Employees.java
+++ tugaskelompok/src/tugaskelompok/Employees.java
@@ -5,7 +5,6 @@
  */
 package tugaskelompok;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,100 +13,55 @@ import java.util.Scanner;
  * @author NAME
  */
 public class Employees {
-    int idEmployee;
-    String namaDepan,namaBelakang,username,password;
-   
-     public static String tambahemployee(){
-          Scanner in = new Scanner(System.in);
-           ArrayList<Integer> id = new ArrayList<Integer>();
-              ArrayList<String> nameLengkap = new ArrayList<String>();
-              ArrayList<String> lastName = new ArrayList<String>();
-              ArrayList<String> username = new ArrayList<String>();
-              ArrayList<String> password = new ArrayList<String>();
-          
-       System.out.println("=====================");
-      
-                    System.out.println("Pilihan(1-3) : 1");
-                    int a=0;
-                    a++;
-                    id.add(a);
-                    System.out.print("Nama Lengkap    : ");
-                    String namaAsli = in.nextLine();
-                    nameLengkap.add(namaAsli);
-                    //menambahkan nama belakang 
-                    String namaAkhir = namaAsli.substring(namaAsli.lastIndexOf(' '));
-                    lastName.add(namaAkhir);
-                    //   String  namaLengkap = in.nextLine();             
-                    System.out.print("Password        : ");
-                    String pas = in.nextLine();  
-                    //   mendapat hashe password;
-                    String passHashe = pas;
-                    String getSalt = BCrypt.gensalt();
-                    String hashePass = BCrypt.hashpw(pas, getSalt);
-                    password.add(hashePass);  
-                    //username
-                    String user = namaAkhir.substring(0, 3);
-                     String[] values = namaAsli.split(" ",2);
-                      String f = values[0];
-                      String userFix = f+user;
-                      //menghilangkan space
-                      userFix = userFix.replaceAll("\\s", "");
-                      username.add(userFix);
-                    
-            
-           for (int i = 0; i < a; i++) {
-             System.out.println("ID = "+id.get(i));
-             System.out.println("Nama Lengkap = "+nameLengkap.get(i));
-             System.out.println("Nama Akhir = "+lastName.get(i));
-               System.out.println("Username = "+ username.get(i));
-             
-             System.out.println("Password : "+password.get(i));
-             
-         }
-        
-                     System.out.println("=====================");
-                    System.out.println("");
-    return "";}
-     
-     //public static showEmployee(ArrayList<>)
-    
-    public int getIdEmployee() {
-        return idEmployee;
+
+    static String[] namaDepan, namaBelakang, username, password;
+    static int[] idEmployee;
+    static ArrayList<Integer> id = new ArrayList<>();
+    static ArrayList<String> nameLengkap = new ArrayList<>();
+    static ArrayList<String> lastName = new ArrayList<>();
+    static ArrayList<String> usernameArr = new ArrayList<>();
+    static ArrayList<String> passwordArr = new ArrayList<String>();
+
+    public static String tambahemployee() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Pilihan(1-3) : 1");
+        int a = id.size();
+        id.add(a++);
+        System.out.print("Nama Lengkap    : ");
+        String namaAsli = in.nextLine();
+        nameLengkap.add(namaAsli);
+        //   menambahkan nama belakang 
+        String namaAkhir = namaAsli.substring(namaAsli.lastIndexOf(' '));
+        lastName.add(namaAkhir);
+        //   String  namaLengkap = in.nextLine();             
+        System.out.print("Password        : ");
+        String pas = in.nextLine();
+        //   mendapat hashe password;
+        String passHashe = pas;
+        String getSalt = BCrypt.gensalt();
+        String hashePass = BCrypt.hashpw(pas, getSalt);
+        passwordArr.add(hashePass);
+        //username
+        String user = namaAkhir.substring(0, 3);
+        String[] values = namaAsli.split(" ", 2);
+        String f = values[0];
+        String userFix = f + user;
+        //menghilangkan space
+        userFix = userFix.replaceAll("\\s", "");
+        usernameArr.add(userFix);
+        System.out.println("=====================");
+        System.out.println("");
+        return "";
     }
 
-    public void setIdEmployee(int idEmployee) {
-        this.idEmployee = idEmployee;
+    public static void lihatemployee() {
+        for (int i = 0; i < id.size(); i++) {
+            System.out.println("ID = " + id.get(i)+1);
+            System.out.println("Nama Lengkap = " + nameLengkap.get(i));
+            System.out.println("Nama Akhir = " + lastName.get(i));
+            System.out.println("Username = " + usernameArr.get(i));
+            System.out.println("Password : " + passwordArr.get(i));
+        }
     }
 
-    public String getNamaDepan() {
-        return namaDepan;
-    }
-
-    public void setNamaDepan(String namaDepan) {
-        this.namaDepan = namaDepan;
-    }
-
-    public String getNamaBelakang() {
-        return namaBelakang;
-    }
-
-    public void setNamaBelakang(String namaBelakang) {
-        this.namaBelakang = namaBelakang;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- tugaskelompok/src/tugaskelompok/Main.java
+++ tugaskelompok/src/tugaskelompok/Main.java
@@ -6,9 +6,6 @@
 package tugaskelompok;
 
 import java.util.Scanner;
-import static tugaskelompok.Employees.tambahemployee;
-
-
 
 /**
  *
@@ -22,55 +19,48 @@ public class Main {
     public static void main(String[] args) {
         Employees employees = new Employees();
         Scanner scan = new Scanner(System.in);
-       
+        
         int inputPilihan;
         boolean pilihan = true;
-
+        
         while (pilihan) {
             System.out.println("=====================");
             System.out.println("1. Add");
             System.out.println("2. View");
             System.out.println("3. Login");
+            System.out.println("4. Exit");
             System.out.println("=====================");
-            System.out.print("Pilihan(1-3) : ");
+            System.out.print("Pilihan(1-4) : ");
             inputPilihan = scan.nextInt();
-
+            
             switch (inputPilihan) {
                 case 1:
-                    System.out.println("========================================"); 
-                    System.out.println("Pilihan(1-3) : 1");
-                    System.out.println("Nama Lengkap    : ");
-                    System.out.println("Password        :");
-                    System.out.println("========================================");
+                    System.out.println("=====================");
+                    Employees.tambahemployee();
                     System.out.println("");
-                    tambahemployee();
-                  
                     break;
                 case 2:
-                     System.out.println("=====================");
-                    System.out.println("Pilihan(1-3) : 2");
-                    System.out.println("Id Karyawan     : ");
-                    System.out.println("Nama Depan      : ");
-                    System.out.println("Nama Belakang   : "+employees.namaBelakang);
-                    System.out.println("Username        : ");
-                   System.out.println("=====================");
+                    System.out.println("=====================");
+                    System.out.println("Pilihan(1-4) : 2");
+                    Employees.lihatemployee();
+                    System.out.println("=====================");
                     System.out.println("");
                     break;
                 case 3:
-                   System.out.println("=====================");
-                    System.out.println("Pilihan(1-3) : 3");
+                    System.out.println("=====================");
+                    System.out.println("Pilihan(1-4) : 3");
                     System.out.println("Username    : ");
                     System.out.println("Password    : ");
-                  System.out.println("=====================");
+                    System.out.println("=====================");
                     System.out.println("");
+                    break;
+                case 4:
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Data tidak di temukan silahkan pilih 1-3");
             }
         }
     }
-   
-
-
-
+    
 }
